c0795f31bb1d2fc8f40e767262e0987d5f66f013
kensuke <kensuke@impeldown>
2015-11-11 15:00:25 +0800

    License lines are unified.

--- manhattan/anisotime/About.java
+++ manhattan/anisotime/About.java
@@ -1,45 +1,20 @@
 package anisotime;
 
+import java.awt.GraphicsEnvironment;
+
 /**
  * @author kensuke
  * @since 2015/8/19
  * 
- * @version 0.0.2
+ * @version 0.0.2.1
  * 
  */
 final class About extends javax.swing.JFrame {
 
 	private static final long serialVersionUID = -8583520488520194322L;
 
-	/**
-	 * Creates new form AboutANISOtime
-	 */
 	About() {
 		initComponents();
-	}
-
-	static final void printLicense() {
-
-		System.out.print("ANISOtime " + ANISOtime.version + " (" + ANISOtime.codename + ") ");
-		System.out.println("Copyright \u00a9 2015 Kensuke Konishi");
-		// Copyright [yyyy] [name of copyright owner]
-		System.out.println();
-		System.out.println("Licensed under the Apache License, Version 2.0 (the \"License\")");
-		System.out.println("you may not use this file except in compliance with the License.");
-		System.out.println("You may obtain a copy of the License at");
-
-		System.out.println("\n\thttp://www.apache.org/licenses/LICENSE-2.0\n");
-
-		System.out.println("Unless required by applicable law or agreed to in writing, software "
-				+ "distributed under the License is distributed on an \"AS IS\" BASIS, "
-				+ "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.");
-		System.out.println("See the License for the specific language governing permissions and "
-				+ "limitations under the License.");
-
-	}
-
-	static final void showLicense() {
-		About.main(null);
 	}
 
 	/**
@@ -57,7 +32,7 @@ final class About extends javax.swing.JFrame {
 		jTextArea1.setColumns(20);
 		jTextArea1.setRows(5);
 		jScrollPane1.setViewportView(jTextArea1);
-		setLicenseLines();
+		jTextArea1.setText(line);
 		jTextArea1.setLineWrap(true);
 		jTextArea1.setEditable(false);
 		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -72,21 +47,18 @@ final class About extends javax.swing.JFrame {
 						.addContainerGap()));
 
 		pack();
-		setSize(300, 300);
 		java.awt.EventQueue.invokeLater(() -> jScrollPane1.getVerticalScrollBar().setValue(0));
 	}// </editor-fold>
 
-	private void setLicenseLines() {
-		String line = "  ANISOtime "+ANISOtime.version+" ("+ANISOtime.codename+")\n\nCopyright © 2015 Kensuke Konishi\n"
-				+ "Licensed under the Apache License, Version 2.0 (the \"License\")\n"
-				+ "You may not use this file except in compliance with the License.\n"
-				+ "You may obtain a copy of the License at\n\n" + "http://www.apache.org/licenses/LICENSE-2.0\n\n"
-				+ "Unless required by applicable law or agreed to in writing, "
-				+ "software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
-				+ "See the License for the specific language governing permissions and limitations under the License.";
-		jTextArea1.setText(line);
 
-	}
+	private static final String line = "ANISOtime " + ANISOtime.version + " (" + ANISOtime.codename
+			+ ") Copyright © 2015 Kensuke Konishi\n\n"
+			+ "Licensed under the Apache License, Version 2.0 (the \"License\")\n"
+			+ "You may not use this file except in compliance with the License.\n"
+			+ "You may obtain a copy of the License at\n\n" + "\thttp://www.apache.org/licenses/LICENSE-2.0\n\n"
+			+ "Unless required by applicable law or agreed to in writing, "
+			+ "software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+			+ "See the License for the specific language governing permissions and limitations under the License.";
 
 	/**
 	 * @param args
@@ -110,22 +82,20 @@ final class About extends javax.swing.JFrame {
 				}
 			}
 		} catch (ClassNotFoundException ex) {
-			java.util.logging.Logger.getLogger(About.class.getName()).log(java.util.logging.Level.SEVERE, null,
-					ex);
+			java.util.logging.Logger.getLogger(About.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (InstantiationException ex) {
-			java.util.logging.Logger.getLogger(About.class.getName()).log(java.util.logging.Level.SEVERE, null,
-					ex);
+			java.util.logging.Logger.getLogger(About.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (IllegalAccessException ex) {
-			java.util.logging.Logger.getLogger(About.class.getName()).log(java.util.logging.Level.SEVERE, null,
-					ex);
+			java.util.logging.Logger.getLogger(About.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-			java.util.logging.Logger.getLogger(About.class.getName()).log(java.util.logging.Level.SEVERE, null,
-					ex);
+			java.util.logging.Logger.getLogger(About.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 		}
 		// </editor-fold>
-
 		/* Create and display the form */
-		java.awt.EventQueue.invokeLater(() -> new About().setVisible(true));
+		if (!GraphicsEnvironment.isHeadless())
+			java.awt.EventQueue.invokeLater(() -> new About().setVisible(true));
+		else
+			System.out.println(line);
 	}
 
 	// Variables declaration - do not modify
