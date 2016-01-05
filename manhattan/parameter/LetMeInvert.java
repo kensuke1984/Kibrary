c9ae59cec04de0ad7c8a954ea3de66ae073b8ceb
kensuke1984 <kensuke@earth.sinica.edu.tw>
2016-01-05 14:23:37 +0800

    small change

--- manhattan/parameter/LetMeInvert.java
+++ manhattan/parameter/LetMeInvert.java
@@ -17,23 +17,8 @@ import manhattan.inversion.InverseMethodEnum;
 /**
  * Information of {@link manhattan.inversion.LetMeInvert}
  * 
- * @version 0.0.2
- * @since 2013/9/25
  * 
- * @version 0.1.0
- * @since 2013/12/2 イベント情報ファイルを不要に
- * 
- * @version 0.1.1
- * @since 2014/9/7 to Java 8
- * 
- * @since 2014/9/11
- * @version 0.1.2 {@link #checkElements()} installed.
- * 
- * @version 0.1.2.1
- * @since 2015/8/7 {@link IOException}
- * 
- * @version 0.1.3
- * @since 2015/8/8 {@link Path} base
+ * @version 0.1.3.1
  * 
  * @author kensuke
  * 
@@ -69,10 +54,6 @@ public class LetMeInvert extends ParameterFile {
 	 */
 	protected Path stationInformationPath;
 
-//	/**
-//	 * 何番目の解でボルン波形を作るか
-//	 */
-//	protected int[] bornAnswer = { 1, 5, 10 };
 
 	/**
 	 * どうやって方程式を解くか。 cg svd
@@ -92,8 +73,6 @@ public class LetMeInvert extends ParameterFile {
 		String[] alphaParts = reader.getStringArray("alpha");
 		alpha = Arrays.stream(alphaParts).mapToDouble(Double::parseDouble).toArray();
 
-//		String[] bornParts = reader.getStringArray("bornAnswer");
-//		bornAnswer = Arrays.stream(bornParts).mapToInt(Integer::parseInt).toArray();
 
 		String[] inverseMethodsStr = reader.getStringArray("inverseMethodsStr");
 		inverseMethods = Arrays.stream(inverseMethodsStr).map(InverseMethodEnum::valueOf).collect(Collectors.toSet());
@@ -134,8 +113,6 @@ public class LetMeInvert extends ParameterFile {
 			pw.println("stationInformationPath station.inf");
 			pw.println("#double[] alpha it self");
 			pw.println("alpha 1 12.5 25 125");
-//			pw.println("#int[] born answer ");
-//			pw.println("bornAnswer 1 5 10");
 			pw.println("#inverseMethodsStr[] inverse methods (cg, svd) must be capital letters");
 			pw.println("inverseMethodsStr CG SVD ");
 
@@ -169,7 +146,6 @@ public class LetMeInvert extends ParameterFile {
 		parameterSet.add("unknownParameterListPath");
 		parameterSet.add("waveformPath");
 		parameterSet.add("alpha");
-//		parameterSet.add("bornAnswer");
 		parameterSet.add("inverseMethodsStr");
 		return reader.containsAll(parameterSet);
 	}
