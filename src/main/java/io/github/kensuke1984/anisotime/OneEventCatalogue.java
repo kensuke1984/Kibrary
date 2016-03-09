095a0bdd211850ae2df38eab8458c1d5915cd355
kensuke1984 <kensuke@earth.sinica.edu.tw>
2016-03-09 11:04:28 +0800

    initial commit

---
+++ src/main/java/io/github/kensuke1984/anisotime/OneEventCatalogue.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package io.github.kensuke1984.anisotime;
+
+/**
+ * 
+ * Raypath catalogue of an event
+ * 
+ * 
+ * @author kensuke 
+ * @version 0.0.1
+ */
+class OneEventCatalogue {
+
+}
