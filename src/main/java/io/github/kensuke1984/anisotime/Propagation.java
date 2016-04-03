e1c3b16981ebb51c32842f176352a3c8f52d836d
kensuke1984 <kensuke@earth.sinica.edu.tw>
2016-04-03 19:52:04 +0800

    new class

---
+++ src/main/java/io/github/kensuke1984/anisotime/Propagation.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package io.github.kensuke1984.anisotime;
+
+/**
+ * How phases P and S goes in Partitions
+ * 
+ * 
+ * @author kensuke 
+ * @version 0.0.1
+ */
+enum Propagation {
+	PENETRATING, BOUNCING, NOEXIST,
+}
