5f1c3ebaf045712e78059842f1bda20843386288
kensuke1984 <kensuke@earth.sinica.edu.tw>
2016-07-06 11:45:26 +0800

    Deprecation avoid

--- src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
+++ src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
@@ -15,7 +15,7 @@ final class ANISOtime extends TravelTimeGUI {
 	
 	static final String codename = "Promyvion";
 	
-	static final String version ="0.3.8b";
+	static final String version ="0.4b";
 	
 	private static final long serialVersionUID = -4093263118460123169L;
 
