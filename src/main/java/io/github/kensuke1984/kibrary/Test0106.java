e533f2fb80aea4aab8637f7b95ab2dcc47753b7f
yuki0317 <Yuki@dhcp-229-180.eps.s.u-tokyo.ac.jp>
2017-01-06 16:08:28 +0900

    test

---
+++ src/main/java/io/github/kensuke1984/kibrary/Test0106.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package io.github.kensuke1984.kibrary;
+
+/**
+ * @version 0.0.1
+ * @since 2017/01/06
+ * @author Yuki
+ *
+ */
+public class Test0106 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("hihi");
+	}
+
+}
