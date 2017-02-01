9e495ae10fa04d45367ccf72d45f8d69f64211d7
kensuke1984 <kensuke@earth.sinica.edu.tw>
2017-02-01 17:17:18 +0800

    apache lang 3.5

--- src/main/java/io/github/kensuke1984/kibrary/About.java
+++ src/main/java/io/github/kensuke1984/kibrary/About.java
@@ -28,7 +28,7 @@ public final class About extends javax.swing.JFrame {
 
     public static final String CODENAME = "Sahagin";
 
-    public static final String VERSION = "0.4.2.4";
+    public static final String VERSION = "0.4.2.4.1";
 
     private About() {
         super("About Kibrary");
