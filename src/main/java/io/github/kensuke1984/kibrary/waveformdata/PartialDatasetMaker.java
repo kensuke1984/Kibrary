ee67526cc3b3093f75bcebdcde88cba87ce651ac
kensuke1984 <kensuke@earth.sinica.edu.tw>
2017-05-09 10:09:05 +0800

    FBN

--- src/main/java/io/github/kensuke1984/kibrary/waveformdata/PartialDatasetMaker.java
+++ src/main/java/io/github/kensuke1984/kibrary/waveformdata/PartialDatasetMaker.java
@@ -297,6 +297,7 @@ public class PartialDatasetMaker implements Operation {
         Set<Path> bpFolderSet =
                 stationSet.stream().map(s -> bpPath.resolve("0000" + s.getName())).collect(Collectors.toSet());
         Set<EventFolder> eventFolders = Utilities.eventFolderSet(fpPath);
+        System.out.println(modelName.isEmpty() + " a" + modelName + "b");
         if (modelName.isEmpty()) {
             Set<String> possible =
                     bpFolderSet.stream().flatMap(b -> Arrays.stream(b.toFile().listFiles(File::isDirectory)))
@@ -304,15 +305,12 @@ public class PartialDatasetMaker implements Operation {
             if (possible.size() != 1) throw new RuntimeException(
                     "There are no model folder in bp folders or more than one folder. You must specify 'modelName' in the case.");
             String model = possible.iterator().next();
-
             Set<String> possibleNames =
                     eventFolders.stream().flatMap(ef -> Arrays.stream(ef.listFiles(File::isDirectory)))
                             .map(File::getName).collect(Collectors.toSet());
             if (possibleNames.size() != 1) throw new RuntimeException(
                     "There are no model folder in fp folders or more than one folder. You must specify 'modelName' in the case.");
-
             if (!model.equals(possibleNames.iterator().next())) throw new RuntimeException("No valid model folder.");
-
         }
         System.err.println("Model name is set " + modelName);
         if (!eventFolders.stream().map(EventFolder::toPath).map(p -> p.resolve(modelName)).allMatch(Files::exists) ||
