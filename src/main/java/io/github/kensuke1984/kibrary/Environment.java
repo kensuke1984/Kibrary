fbcff810352f849ffbbd6c4fdff0106136362963
kensuke1984 <kensuke@earth.sinica.edu.tw>
2019-04-25 17:44:03 +0800

    .property

--- src/main/java/io/github/kensuke1984/kibrary/Environment.java
+++ src/main/java/io/github/kensuke1984/kibrary/Environment.java
@@ -6,17 +6,18 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.Properties;
 
 /**
  * Runtime environment
  *
  * @author Kensuke Konishi
- * @version 0.1.0
+ * @version 0.1.1
  */
 public class Environment {
-    public final static Path KIBRARY_HOME = Paths.get(System.getProperty("user.home") + "/.Kibrary");
-    public final static Path PROPERTY_FILE = KIBRARY_HOME.resolve(".environment");
+    public final static Path KIBRARY_HOME;
+    public final static Path PROPERTY_FILE;
     private static final Properties PROPERTY = new Properties();
     private static final Properties DEFAULT_PROPERTY = new Properties();
 
@@ -28,6 +29,20 @@ public class Environment {
         DEFAULT_PROPERTY.setProperty("mail", "7-3-1 Hongo, Bunkyo, Tokyo, Japan");
         DEFAULT_PROPERTY.setProperty("phone", "03-5841-4290");
         DEFAULT_PROPERTY.setProperty("fax", "03-5841-8791");
+    }
+
+    static {
+        String home = System.getenv("KIBRARY_HOME");
+        if (Objects.isNull(home)) {
+            KIBRARY_HOME = Paths.get(System.getProperty("user.home")).resolve("Kibrary");
+            System.err.println("Because KIBRARY_HOME is not set, KIBRARY_HOME is assumed to be " + KIBRARY_HOME);
+        } else KIBRARY_HOME = Paths.get(home);
+        try {
+            Files.createDirectories(KIBRARY_HOME);
+        } catch (IOException io) {
+            throw new RuntimeException("Set a proper KIBRARY_HOME.");
+        }
+        PROPERTY_FILE = KIBRARY_HOME.resolve(".property");
     }
 
     private static void copyDefault() {
