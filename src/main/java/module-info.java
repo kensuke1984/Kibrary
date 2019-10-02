82407cda308bc7a20809e8acc09a8d625690a7c7
anselme <anselme@localhost.localdomain>
2019-10-02 13:55:23 +0800

    add method to interpolate ray parameter modify printResults in ANISOtimeCLI

---
+++ src/main/java/module-info.java
@@ -0,0 +1,29 @@
+module Kibrary_interpolate {
+	exports io.github.kensuke1984.kibrary.dsminformation;
+	exports io.github.kensuke1984.kibrary.datarequest;
+	exports io.github.kensuke1984.kibrary.util.globalcmt;
+	exports io.github.kensuke1984.kibrary.external.gmt;
+	exports io.github.kensuke1984.kibrary.firsthandler;
+	exports io.github.kensuke1984.kibrary.util.spc;
+	exports io.github.kensuke1984.anisotime;
+	exports io.github.kensuke1984.kibrary.elasticparameter;
+	exports io.github.kensuke1984.kibrary.external.gnuplot;
+	exports io.github.kensuke1984.kibrary.timewindow;
+	exports io.github.kensuke1984.kibrary.datacorrection;
+	exports io.github.kensuke1984.kibrary.math;
+	exports io.github.kensuke1984.kibrary.selection;
+	exports io.github.kensuke1984.kibrary.inversion;
+	exports io.github.kensuke1984.kibrary.butterworth;
+	exports io.github.kensuke1984.kibrary.external;
+	exports io.github.kensuke1984.kibrary.waveformdata;
+	exports io.github.kensuke1984.kibrary;
+	exports io.github.kensuke1984.kibrary.util.sac;
+	exports io.github.kensuke1984.kibrary.inversion.montecarlo;
+	exports io.github.kensuke1984.kibrary.math.geometry;
+	exports io.github.kensuke1984.kibrary.stacking;
+	exports io.github.kensuke1984.kibrary.util;
+
+	requires java.datatransfer;
+	requires java.desktop;
+	requires java.logging;
+}
