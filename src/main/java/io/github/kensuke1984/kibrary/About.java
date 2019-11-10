c3f472abff9bfd18541f7f39849a26f2063521e2
kensuke1984 <kensuke@earth.sinica.edu.tw>
2019-11-10 22:32:41 +0800

    Version number

--- src/main/java/io/github/kensuke1984/kibrary/About.java
+++ src/main/java/io/github/kensuke1984/kibrary/About.java
@@ -23,13 +23,13 @@ import java.util.logging.Logger;
 public final class About extends javax.swing.JFrame {
     public static final String EMAIL_ADDRESS = "kensuke@earth.sinica.edu.tw";
     public static final String CODENAME = "Titan";
-    public static final String VERSION = "0.4.5.2";
+    public static final String VERSION = "0.4.5.3";
     private static final String line;
 
     /**
-     * 2019/11/5
+     * 2019/11/10
      */
-    private static final long serialVersionUID = -4275797408023091452L;
+    private static final long serialVersionUID = 7017891876918736278L;
 
     static {
         try {
