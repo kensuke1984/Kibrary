7e6106f89ef5eab915dfd5cc3f7c4fbb5a2011a0
kensuke1984 <kensuke@earth.sinica.edu.tw>
2019-11-20 15:04:59 +0800

    gradle ->6.0

--- src/main/java/io/github/kensuke1984/kibrary/About.java
+++ src/main/java/io/github/kensuke1984/kibrary/About.java
@@ -23,7 +23,7 @@ import java.util.logging.Logger;
 public final class About extends javax.swing.JFrame {
     public static final String EMAIL_ADDRESS = "kensuke@earth.sinica.edu.tw";
     public static final String CODENAME = "Titan";
-    public static final String VERSION = "0.4.5.3";
+    public static final String VERSION = "0.4.5.4";
     private static final String line;
 
     /**
