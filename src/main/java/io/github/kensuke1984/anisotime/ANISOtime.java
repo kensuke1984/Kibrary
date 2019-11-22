b489aba3dd7d1f92cbb9c7d7d7dc98bb1b58f68f
kensuke1984 <kensuke@earth.sinica.edu.tw>
2019-11-22 14:16:05 +0800

    No existence wave.

--- src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
+++ src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
@@ -18,7 +18,7 @@ final class ANISOtime {
 
     static final String CODENAME = "Limsa";
 
-    static final String VERSION = "1.3.1.3b";
+    static final String VERSION = "1.3.1.4b";
 
     private ANISOtime() {
     }
