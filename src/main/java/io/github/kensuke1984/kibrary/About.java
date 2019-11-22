7438913a0e09060b6ea91fddbd4467688be12a27
kensuke1984 <kensuke@earth.sinica.edu.tw>
2019-11-22 14:19:43 +0800

    ANISOtime update

--- src/main/java/io/github/kensuke1984/kibrary/About.java
+++ src/main/java/io/github/kensuke1984/kibrary/About.java
@@ -23,13 +23,13 @@ import java.util.logging.Logger;
 public final class About extends javax.swing.JFrame {
     public static final String EMAIL_ADDRESS = "kensuke@earth.sinica.edu.tw";
     public static final String CODENAME = "Titan";
-    public static final String VERSION = "0.4.5.5";
+    public static final String VERSION = "0.4.5.6";
     private static final String line;
 
     /**
-     * 2019/11/21
+     * 2019/11/22
      */
-    private static final long serialVersionUID = -43990234831915844L;
+    private static final long serialVersionUID = -4011742888258139705L;
 
     static {
         try {
