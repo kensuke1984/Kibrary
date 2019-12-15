45930de3cbb689ed4e57d201eb0aa33413b8ce34
kensuke1984 <kensuke@earth.sinica.edu.tw>
2019-12-15 20:39:37 +0800

    Bug fixed.

--- src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
+++ src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
@@ -18,7 +18,7 @@ final class ANISOtime {
 
     static final String CODENAME = "Little Ala Mhigo";
 
-    static final String VERSION = "1.3.4b";
+    static final String VERSION = "1.3.4.1b";
 
     private ANISOtime() {
     }
