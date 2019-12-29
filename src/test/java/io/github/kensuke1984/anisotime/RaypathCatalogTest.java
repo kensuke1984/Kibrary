cce3d36071de675423dd8f23a425aa5ddc9367c6
kensuke1984 <kensuke@earth.sinica.edu.tw>
2019-12-29 18:21:19 +0800

    .

--- src/test/java/io/github/kensuke1984/anisotime/RaypathCatalogTest.java
+++ src/test/java/io/github/kensuke1984/anisotime/RaypathCatalogTest.java
@@ -10,12 +10,20 @@ import java.io.*;
  */
 class RaypathCatalogTest {
     private static void createCheck() throws IOException, ParseException {
-        RaypathCatalog prem = RaypathCatalog.prem();
+        RaypathCatalog prem = RaypathCatalog.iprem();
         for (Raypath raypath : prem.getRaypaths()) {
-            System.out.println(raypath.getRayParameter());
+            double eventR = 6371-100;
+//            System.out.println(raypath.getRayParameter()+" "+Math.toDegrees(raypath.computeDelta(Phase.P,eventR))+" "+
+//                    raypath.computeT(Phase.P,eventR));
+
         }
-//        RaypathCatalog isoPrem = RaypathCatalog.iprem();
-//        RaypathCatalog ak135 = RaypathCatalog.ak135();
+        System.out.println( (prem.searchPath(Phase.P, 6371, Math.toRadians(50), false)[0].computeT(Phase.P, 6371)));
+        System.exit(0);
+        //261.6192175422948 94.01490821552146 798.1703394543351
+        // 337.7542692276028 73.18767642655352 691.0307993093318
+//478.85555933255523 39.05726961249951 431.16559569122245
+        RaypathCatalog isoPrem = RaypathCatalog.iprem();
+        RaypathCatalog ak135 = RaypathCatalog.ak135();
     }
 
 
