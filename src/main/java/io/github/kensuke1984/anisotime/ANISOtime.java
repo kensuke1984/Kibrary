883c05a6c2747f1ddd65769e6b51e3170fe5a53b
kensuke1984 <kensuke@earth.sinica.edu.tw>
2020-02-09 17:26:55 +0800

    version

--- src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
+++ src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
@@ -18,7 +18,7 @@ final class ANISOtime {
 
     static final String CODENAME = "Eulmore";
 
-    static final String VERSION = "1.3.7b";
+    static final String VERSION = "1.3.7.1b";
 
     private ANISOtime() {
     }
