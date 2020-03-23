83c84ab8d66bc56aaf4e02f665eb9b89c0e95b04
kensuke1984 <kensuke@earth.sinica.edu.tw>
2020-03-23 15:31:44 +0800

    country

--- src/main/java/io/github/kensuke1984/kibrary/About.java
+++ src/main/java/io/github/kensuke1984/kibrary/About.java
@@ -23,7 +23,7 @@ import java.util.logging.Logger;
 public final class About extends javax.swing.JFrame {
     public static final String EMAIL_ADDRESS = "kensuke@earth.sinica.edu.tw";
     public static final String CODENAME = "Garuda";
-    public static final String VERSION = "0.4.8.1";
+    public static final String VERSION = "0.4.8.2";
     private static final String line;
 
     /**
