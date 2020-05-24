cb44b0be07dc46bcc5363aad59409858ff03318b
kensuke1984 <kensuke@earth.sinica.edu.tw>
2020-05-24 14:44:13 +0800

    Catalog reuse

--- src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
+++ src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
@@ -18,7 +18,7 @@ final class ANISOtime {
 
     static final String CODENAME = "Eulmore";
 
-    static final String VERSION = "1.3.7.13b";
+    static final String VERSION = "1.3.7.14b";
 
     private ANISOtime() {
     }
