45aef5097f059b64cf04f1226942764efaf549f8
kensuke1984 <kensuke@earth.sinica.edu.tw>
2020-06-08 15:56:33 +0800

    bug related to 'p,s' fixed

--- src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
+++ src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
@@ -18,7 +18,7 @@ final class ANISOtime {
 
     static final String CODENAME = "Eulmore";
 
-    static final String VERSION = "1.3.7.19b";
+    static final String VERSION = "1.3.7.20b";
 
     private ANISOtime() {
     }
