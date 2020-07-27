aa407c6c8d16af36af92fce31ce52877f645c165
kensuke1984 <kensuke@earth.sinica.edu.tw>
2020-07-27 19:27:14 +0800

    version

--- src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
+++ src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
@@ -18,7 +18,7 @@ final class ANISOtime {
 
     static final String CODENAME = "Taoyuan";
 
-    static final String VERSION = "1.3.8b";
+    static final String VERSION = "1.3.8.1b";
 
     private ANISOtime() {
     }
