a071b2e6bf6905c8ea2ff8cc8e3ca1b0c8b409c4
kensuke1984 <kensuke@earth.sinica.edu.tw>
2020-07-27 19:06:27 +0800

    output slimmed

--- src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
+++ src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
@@ -16,9 +16,9 @@ import java.util.logging.Logger;
  */
 final class ANISOtime {
 
-    static final String CODENAME = "Eulmore";
+    static final String CODENAME = "Taoyuan";
 
-    static final String VERSION = "1.3.7.24b";
+    static final String VERSION = "1.3.8b";
 
     private ANISOtime() {
     }
