62f690addb140e2c5f48c93565072afd05c9bd17
Kensuke Konishi <kensuke1984@hotmail.co.jp>
2020-08-01 16:35:53 +0800

    auto update

--- src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
+++ src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
@@ -1,10 +1,19 @@
 package io.github.kensuke1984.anisotime;
 
+import io.github.kensuke1984.kibrary.util.Utilities;
 import org.apache.commons.cli.UnrecognizedOptionException;
 
 import javax.swing.*;
 import javax.swing.UIManager.LookAndFeelInfo;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,15 +27,32 @@ final class ANISOtime {
 
     static final String CODENAME = "Taoyuan";
 
-    static final String VERSION = "1.3.8.4b";
+    static final String VERSION = "1.3.8.5b";
 
     private ANISOtime() {
+    }
+
+    //anisotime.bat
+    private static final String WINDOWS_SCRIPT_URL;
+    //anisotime (shell script)
+    private static final String UNIX_SCRIPT_URL;
+
+    static {
+        WINDOWS_SCRIPT_URL = "https://bit.ly/2QUnqJr";
+        UNIX_SCRIPT_URL = "https://bit.ly/2Xdq5QI";
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        try {
+            downloadANISOtime();
+        } catch (IOException e) {
+            System.err.println("Can't check for updates, could be due to Off-Line.");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         if (args.length != 0) try {
             ANISOtimeCLI.main(args);
             return;
@@ -57,6 +83,24 @@ final class ANISOtime {
             Logger.getLogger(ANISOtime.class.getName()).log(Level.SEVERE, null, ex);
         }
         SwingUtilities.invokeLater(() -> new ANISOtimeGUI().setVisible(true));
+    }
+
+    private static void downloadANISOtime() throws IOException, NoSuchAlgorithmException, URISyntaxException {
+        Path localPath = Paths.get(ANISOtime.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+        if (Files.isDirectory(localPath))
+            return;
+        String localSum = Utilities.checksum(localPath, "SHA-256");
+        Path path = Utilities.download(new URL(System.getProperty("os.name").contains("Windows") ? WINDOWS_SCRIPT_URL : UNIX_SCRIPT_URL));
+        String cloudSum = Utilities.checksum(path, "SHA-256");
+        if (localSum.equals(cloudSum))
+            return;
+        Files.move(path, localPath.resolveSibling("latest_anisotime"), StandardCopyOption.REPLACE_EXISTING);
+        try {
+            JOptionPane.showMessageDialog(null, "Software update is found. ANISOtime restarts.");
+        } catch (HeadlessException e) {
+            System.err.println("Software update is found. ANISOtime restarts.");
+        }
+        System.exit(55);
     }
 
 }
