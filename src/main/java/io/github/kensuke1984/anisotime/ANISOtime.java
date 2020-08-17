de65cdec6c3096d65be9f8a8e77009023e2a546f
kensuke <kensuke1984@hotmail.co.jp>
2020-08-17 15:25:08 +0800

    End user license agreement gui

--- src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
+++ src/main/java/io/github/kensuke1984/anisotime/ANISOtime.java
@@ -30,7 +30,7 @@ final class ANISOtime {
 
     static final String CODENAME = "Owase";
 
-    static final String VERSION = "1.3.8.13b";
+    static final String VERSION = "1.3.8.14b";
 
     private ANISOtime() {
     }
